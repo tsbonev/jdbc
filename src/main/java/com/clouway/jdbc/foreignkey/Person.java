9496fa4050bb8a9ac8f9a651731c27b2c0ec3363
Tsvetozar Bonev <tsbonev@gmail.com>
2018-06-21 12:23:17 +0300

    Add inserting person and trip

---
+++ src/main/java/com/clouway/jdbc/foreignkey/Person.java
@@ -0,0 +1,55 @@
+package com.clouway.jdbc.foreignkey;
+
+public class Person {
+
+    private int id;
+    private String fname;
+    private String egn;
+    private int age;
+    private String email;
+
+    public Person(){}
+
+    public Person(String fname, String egn, int age, String email) {
+        this.fname = fname;
+        this.egn = egn;
+        this.age = age;
+        this.email = email;
+    }
+
+    public int getId(){
+        return id;
+    }
+
+    public String getFname() {
+        return fname;
+    }
+
+    public void setFname(String fname) {
+        this.fname = fname;
+    }
+
+    public String getEgn() {
+        return egn;
+    }
+
+    public void setEgn(String egn) {
+        this.egn = egn;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
---
+++ src/main/java/com/clouway/jdbc/foreignkey/Trip.java
@@ -0,0 +1,57 @@
+package com.clouway.jdbc.foreignkey;
+
+import java.sql.Date;
+
+public class Trip {
+
+    private int id;
+    private String egn;
+    private String city;
+    private Date arrival;
+    private Date departure;
+
+    public Trip(){}
+
+    public Trip(String egn, String city, Date arrival, Date departure) {
+        this.egn = egn;
+        this.city = city;
+        this.arrival = arrival;
+        this.departure = departure;
+    }
+
+    public int getId(){
+        return id;
+    }
+
+    public String getEgn() {
+        return egn;
+    }
+
+    public void setEgn(String egn) {
+        this.egn = egn;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public Date getArrival() {
+        return arrival;
+    }
+
+    public void setArrival(Date arrival) {
+        this.arrival = arrival;
+    }
+
+    public Date getDeparture() {
+        return departure;
+    }
+
+    public void setDeparture(Date departure) {
+        this.departure = departure;
+    }
+}
