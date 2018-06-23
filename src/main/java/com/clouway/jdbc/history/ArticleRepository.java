2d9a30a2b80ba181bb7604c2c530a2330e00f562
Tsvetozar Bonev <tsbonev@gmail.com>
2018-06-23 20:00:16 +0300

    Add batch insert

--- src/main/java/com/clouway/jdbc/history/ArticleRepository.java
+++ src/main/java/com/clouway/jdbc/history/ArticleRepository.java
@@ -2,13 +2,22 @@ package com.clouway.jdbc.history;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ArticleRepository {
 
     private static ArticleRepository instance;
 
     private static Connection conn;
+
+    private int perPage = 1;
+
+    public void setPerPage(int perPage){
+        this.perPage = perPage;
+    }
 
     public static ArticleRepository getInstance(Connection conn) {
 
@@ -26,6 +35,86 @@ public class ArticleRepository {
     private ArticleRepository(Connection conn) {
 
         this.conn = conn;
+
+    }
+
+    public void dropTable(){
+
+        try {
+            PreparedStatement drop = conn.prepareStatement("DROP TABLE article");
+            PreparedStatement dropHistory = conn.prepareStatement("DROP TABLE article_history");
+            drop.execute();
+            dropHistory.execute();
+
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+
+    }
+
+    public List<Article> getHistoryPage(int pageNum){
+
+        try {
+            PreparedStatement get = conn.prepareStatement("SELECT * FROM article_history" +
+                    " LIMIT ? OFFSET ?");
+
+            get.setInt(1, this.perPage);
+            get.setInt(2, this.perPage * (pageNum - 1));
+
+            ResultSet resultSet = get.executeQuery();
+
+            List<Article> articleList = new ArrayList<>();
+
+            while (resultSet.next()){
+                Article article = new Article();
+                article.setId(resultSet.getInt("id"));
+                article.setContent(resultSet.getString("content"));
+                articleList.add(article);
+            }
+
+            return articleList;
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    public void addBatch(List<Article> articleList){
+
+        try {
+
+            PreparedStatement add = conn.prepareStatement("INSERT INTO article(" +
+                    "content)" +
+                    " VALUES (?)");
+
+            PreparedStatement history = conn.prepareStatement("INSERT INTO article_history(" +
+                    "content)" +
+                    " VALUES (?)");
+
+            int rowCount = 0;
+
+            for (Article article : articleList) {
+
+                rowCount++;
+
+                add.setString(1, article.getContent());
+                add.addBatch();
+                history.setString(1, article.getContent());
+                add.addBatch();
+
+                if(rowCount % 1000 == 0 || rowCount >= articleList.size()){
+                    add.executeLargeBatch();
+                    history.executeLargeBatch();
+                }
+
+            }
+
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+
 
     }
 
--- src/test/java/com/clouway/jdbc/history/testHistory.java
+++ src/test/java/com/clouway/jdbc/history/testHistory.java
@@ -1,4 +1,92 @@
 package com.clouway.jdbc.history;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.CoreMatchers.is;
+
 public class testHistory {
+
+    ArticleRepository repo;
+
+    @Before
+    public void setUp(){
+        repo = ArticleRepository.getInstance(Demo.getConnection());
+    }
+
+    @After
+    public void cleanUp(){
+
+        repo.dropTable();
+    }
+
+    @Test
+    public void paginateHistory(){
+
+        repo.createArticleTable();
+        repo.setPerPage(3);
+
+        for(int i = 0; i < 9; i++){
+            String content = "This article is numbered " + i;
+            Article article = new Article(content);
+            repo.addArticle(article);
+        }
+
+        List<Article> articleList = repo.getHistoryPage(2);
+
+        assertThat(articleList.size(), is(3));
+        assertThat(articleList.get(0).getContent(), is("This article is numbered 3"));
+
+
+    }
+
+    @Ignore
+    @Test
+    public void addOneByOne(){
+
+        repo.createArticleTable();
+
+        for (int i = 0; i < 100000; i++){
+
+            String content = "This article is numbered " + i;
+            Article article = new Article(content);
+            repo.addArticle(article);
+        }
+
+        //21 seconds 482 ms for 10k
+        //3 minutes 22 seconds 401 ms for 100k
+
+    }
+
+    @Ignore
+    @Test
+    public void addInABatch(){
+
+        repo.createArticleTable();
+
+        List<Article> articleList = new ArrayList<>();
+
+        for (int i = 0; i < 100000; i++){
+
+            String content = "This article is numbered " + i;
+            Article article = new Article(content);
+            articleList.add(article);
+        }
+
+        repo.addBatch(articleList);
+
+        //13 seconds 800 ms for 10k
+        //2 minutes 6 seconds 367 ms for 100k
+
+        //at 1.17 seconds per 1000 inserts 1.000.000 would take 16 minutes
+
+    }
+
+
 }
